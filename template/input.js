66e54b569df5ff4dd8c653a51795945a2335ffe7
Alexey Ershov <aleksey.ershv@gmail.com>
2022-12-08 08:20:34 +0100

    fixed input parser

--- template/input.js
+++ template/input.js
@@ -1,8 +1,8 @@
 const path = require('path');
 const fs = require('fs');
 
-const input = fs.readFileSync(path.join(__dirname, 'input.txt'), 'utf8').toString().split('\r\n');
-const testInput = fs.readFileSync(path.join(__dirname, 'testInput.txt'), 'utf-8').toString().split('\r\n');
+const input = fs.readFileSync(path.join(__dirname, 'input.txt'), 'utf8').toString().trim().split('\r\n');
+const testInput = fs.readFileSync(path.join(__dirname, 'testInput.txt'), 'utf-8').toString().trim().split('\r\n');
 
 module.exports = {
 	input,
